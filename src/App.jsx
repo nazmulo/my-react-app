c731c66d5dd79be9609095f2c08d2a3fa3fed415
nazmulo <itsnaazmul@gmail.com>
2026-09-25 23:41:26 +0600

    add componant to finall componant

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1>hello react</h1>
+      <h1>Vite + React</h1>
+      <Pet></Pet>
+      <Person></Person>
+      <Sports></Sports>
+      <Person></Person>
     </>
   )
 };
